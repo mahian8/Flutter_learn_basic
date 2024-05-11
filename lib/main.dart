7afc3aea2e81c56c9dd2efa81687c833e4cd4ba1
mahian <mahian8112@gmail.com>
2024-05-11 15:49:09 +0800

    Untill chapter 18

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,59 @@
+import 'dart:ui_web';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
-st
+
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String buttonName = 'Click';
+  int currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('App Title'),
+        ),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              setState(() {
+                buttonName = 'Clicked';
+              });
+            },
+            child: Text(buttonName),
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              label: 'Home',
+              icon: Icon(Icons.home),
+            ),
+            BottomNavigationBarItem(
+              label: 'Settings',
+              icon: Icon(Icons.settings),
+            )
+          ],
+          currentIndex: currentIndex,
+          onTap: (int index) {
+            setState(() {
+              currentIndex = index;
+            });
+          },
+        ),
+      ),
+    );
+  }
+}
